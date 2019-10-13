20196d8d2d19de0a27b317156eab6ad2c0cbc650
kentify <52382580+kentify@users.noreply.github.com>
2019-10-13 16:56:02 +0800

    reverted
    asdasdasd

--- getip.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
-echo $ip;
-?>
